0495f0bdad14aa62d739a70a59324aceeec53ab7
Bupal Chowdary <136565586+Bupalchow@users.noreply.github.com>
2023-10-31 19:02:02 +0530

    Delete src directory

--- src/component/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./button.css"
-export default function Button() {
-  return (
-    <div>
-      <button className='btn'>Find A Orphanage</button>
-    </div>
-  )
-}
