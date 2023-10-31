e92541e5cf0bf219d27333feb50a321ec62c7494
Bupal Chowdary <136565586+Bupalchow@users.noreply.github.com>
2023-10-31 19:07:59 +0530

    Update Home.js

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Button from "../component/Button";
 import Heading from "../component/Heading";
-import Nav from "../component/Nav";
 import "./Home.css"
 function Home() {
   return (
     <>
     <div id="main">
       <div id="gradient">
-        <Nav color="white"/>
         <Heading color = "white" size="4vw" title="Celebrate Your Family & Corporate Social Events at Free of Cost with a Social Impact." padding="15vh 25vw" />
         <Button/>
       </div>
